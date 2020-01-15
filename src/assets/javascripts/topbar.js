8ebd6b2ccbb3cc8f6f3a9d03a9cadba968d9312f
sandrine <sandrinezocca@gmail.com>
2020-01-15 16:39:16 +0100

    🧾  get form data

--- src/assets/javascripts/topbar.js
+++ src/assets/javascripts/topbar.js
@@ -1,1 +1,1 @@
-console.log('tobbar');
+console.log('topbar');
--- src/form/form.js
+++ src/form/form.js
@@ -1,4 +1,20 @@
 import "../assets/styles/styles.scss"
 import "./form.scss";
 
-console.log("form");
+// create a reference for the form
+const form = document.querySelector('form');
+
+// add a listener to the submit event of the form
+form.addEventListener('submit', event => {
+  // prevents reloading the page
+  event.preventDefault();
+  // use the FormData object to parse all form fields
+  const formData = new FormData(form);
+  // returns an iterable of key / value pairs
+  const entries = formData.entries();
+  // Object.fromEntries transforms an iterable pair of key / value pairs into a javascript object
+  const article = Object.fromEntries(entries);
+  // convert javascript object to json 
+  const json = JSON.stringify(article);
+  
+})
