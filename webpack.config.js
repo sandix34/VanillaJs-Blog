6540f4cc2f560b98a6b756778def98a931d08fcd
sandrine <sandrinezocca@gmail.com>
2020-01-15 13:15:58 +0100

    📦  use of assets with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const path = require("path");
 // HtmlWebpackPlugin simplifies creation of HTML files to serve your webpack bundles
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+// Copies individual files or entire directories, which already exist, to the build directory.
+const CopyPlugin = require("copy-webpack-plugin");
+// A webpack plugin to remove/clean your build folder(s)
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
     main: path.join(__dirname, "src/index.js"),
-    form: path.join(__dirname, "src/form/form.js")
+    form: path.join(__dirname, "src/form/form.js"),
+    topbar: path.join(__dirname, "src/assets/javascripts/topbar.js")
   },
   output: {
     path: path.join(__dirname, "dist"),
@@ -25,16 +30,20 @@ module.exports = {
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: path.join(__dirname, "./src/index.html"),
-      chunks: ["main"]
+      chunks: ["main", "topbar"]
     }),
     new HtmlWebpackPlugin({
       filename: 'form.html',
       template: path.join(__dirname, "./src/form/form.html"),
-      chunks: ["form"]
-    })
+      chunks: ["form", "topbar"]
+    }),
+    new CopyPlugin([
+      { from: './src/assets/images/*', to: 'assets/images', flatten: true }
+    ])
   ],
   stats: "minimal",
   devtool: "source-map",
