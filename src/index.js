6c83ebd83230877591e69c578142c14dc1291b03
sandrine <sandrinezocca@gmail.com>
2020-01-28 13:57:10 +0100

    ⚙️ sort items by date

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import "./index.scss";
 
 const articleContainerElement = document.querySelector(".articles-container");
 const categoriesContainerElement = document.querySelector(".categories");
+const selectElement = document.querySelector("select");
 let filter;
 let articles;
+let sortBy = 'desc';
+
+selectElement.addEventListener('change', () => {
+  sortBy = selectElement.value;
+  fetchArticle();
+})
 
 const createArticles = () => {  
   const articlesDOM = articles.filter((article) => {
@@ -70,7 +77,10 @@ const createArticles = () => {
 const displayMenuCategories = (categoriesArr) => {
   const liElements = categoriesArr.map( categoryElem => {
     const li = document.createElement('li');
-    li.innerHTML = `<li>${ categoryElem[0] }<strong>(${ categoryElem[1] })</strong></li>`;
+    li.innerHTML = `${ categoryElem[0] }<strong>(${ categoryElem[1] })</strong>`;
+    if (categoryElem[0] === filter) {
+      li.classList.add('active');
+    }
     li.addEventListener('click', () => {
       if (filter === categoryElem[0]) {
         filter = null;
@@ -111,7 +121,7 @@ const createMenuCategories = () => {
 
 const fetchArticle = async () => {
   try {
-    const response = await fetch("https://restapi.fr/api/article");
+    const response = await fetch(`https://restapi.fr/api/article?sort=createdAt:${ sortBy }`);
     articles = await response.json();
     console.log(articles);
     createArticles();
